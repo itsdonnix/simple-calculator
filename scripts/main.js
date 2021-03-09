175b79277f0f7f19d058d6d117d5cab309c104aa
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-09 17:16:36 +0800

    Add script
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

---
+++ scripts/main.js
@@ -0,0 +1,76 @@
+(function () {
+  ("use strict");
+
+  // States
+  let firstInit = true;
+  let lastOperator = 0;
+  let result = 0;
+
+  // DOM Elements
+  const buttons = {};
+  const output = document.querySelector("output");
+  const buttonsWrapper = document.querySelector(".buttons");
+
+  buttonsWrapper.querySelectorAll("button").forEach((button) => {
+    buttons[button.id] = button;
+
+    // Num buttons
+    if (!isNaN(button.innerText)) {
+      button.addEventListener("click", onNumButtonClicked);
+    } else if (button.id === "button-clear") {
+      button.addEventListener("click", onButtonClearClicked);
+    } else if (button.id === "button-equal") {
+      button.addEventListener("click", onButtonEqualClicked);
+    } else {
+      // Operator buttons
+      button.addEventListener("click", onOperatorButtonClicked);
+    }
+  });
+
+  /** @param {MouseEvent} event */
+  function onNumButtonClicked(event) {
+    const text = event.target.innerText;
+    if (firstInit) {
+      output.textContent = text;
+      firstInit = false;
+    } else {
+      output.textContent += text;
+    }
+  }
+
+  /** @param {MouseEvent} event */
+  function onOperatorButtonClicked(event) {
+    result = +output.innerText;
+    lastOperator = +event.target.dataset.opId;
+    clearLcd();
+  }
+
+  /** @param {MouseEvent} event */
+  function onButtonEqualClicked(event) {
+    if (lastOperator == 0) return;
+    if (lastOperator === 1) {
+      result = result + +output.innerText;
+    } else if (lastOperator === 2) {
+      result = result - +output.innerText;
+    } else if (lastOperator === 3) {
+      result = result * +output.innerText;
+    } else if (lastOperator === 4) {
+      result = result / +output.innerText;
+    }
+    clearLcd();
+    lastOperator = 0;
+    output.innerText = result;
+  }
+
+  /** @param {MouseEvent} event */
+  function onButtonClearClicked(event) {
+    output.innerText = 0;
+    result = 0;
+    lastOperator = 0;
+    firstInit = true;
+  }
+
+  function clearLcd() {
+    if (!firstInit) output.innerText = "";
+  }
+})();
